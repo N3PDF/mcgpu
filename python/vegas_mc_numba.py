21d49e08d4a0feecd663e1218fa490d78761a10b
Stefano Carrazza <stefano.carrazza@cern.ch>
2019-08-29 10:36:22 +0200

    final adjustments

--- python/vegas_mc_numba.py
+++ python/vegas_mc_numba.py
@@ -7,75 +7,18 @@ import numba as nb
 BINS_MAX = 50
 
 
-@nb.njit(nb.float64(nb.float64[:],nb.int64[:],nb.int64,nb.int64[:],nb.int64,nb.float64[:],nb.float64[:],nb.float64[:,:]))
-def random_point(x, sbin, dim, box, bins, xl, delx, sxi):
-    vol = 1
-    for i in range(dim):
-        z = (box[i] + np.random.uniform(0,1)) * bins
-        k = nb.int64(z)
-        sbin[i] = k
-        if z == 0:
-            bin_width = sxi[1, i]
-            y = z * bin_width
-        else:
-            bin_width = sxi[k+1, i] - sxi[k, i]
-            y = sxi[k, i] + (z - k) * bin_width
-        x[i] = xl[i] + y * delx[i]
-        vol *= bin_width
-    return vol
-
-
-@nb.njit(nb.int64(nb.int64,nb.int64,nb.int64,nb.float64[:,:],nb.float64[:]))
-def resize_grid(dim, sbins, bins, sxi, sxin):
-    """Resize grid maintaining the density distribution"""
-    pts_per_bin = sbins / bins
-    for j in range(dim):
-        i = 1
-        xnew = 0
-        dw = 0
-
-        for k in range(1, sbins+1):
-            dw += 1.0
-            xold = xnew
-            xnew = sxi[k, j]
-
-            while dw > pts_per_bin:
-                dw -= pts_per_bin
-                sxin[i] = xnew - (xnew - xold) * dw
-                i += 1
-
-        for k in range(1, bins):
-            sxi[k, j] = sxin[k]
-
-        sxi[bins, j] = 1
-
-    return bins
-
-
-@nb.njit(nb.int64(nb.int64,nb.int64[:],nb.int64))
-def change_box_coord(dim, box, boxes):
-    j = dim - 1
-    ng = boxes
-    while j >= 0:
-        box[j] = (box[j] + 1) % ng
-        if box[j] != 0:
-            return 1
-        j -= 1
-    return 0
-
-
 # vegas MC implementation
 @nb.njit(
     nb.types.UniTuple(nb.float64,4)(
         nb.int64,nb.float64[:],nb.float64[:],nb.int64,nb.int64,
         nb.int64,nb.float64[:],nb.float64[:,:],nb.float64[:],nb.float64,
-        nb.float64[:],nb.float64[:,:],nb.int64[:],nb.int64[:],nb.float64,
+        nb.float64[:,:],nb.int64[:],nb.int64[:],
         nb.int64,nb.float64,nb.int64,nb.float64
         )
     )
 def vegas(dim, xl, xu, calls, stage, sbins, sdelx,
-          sxi, sxin, svol, sx, sd, sbox, sbin, sjac,
-          sboxes, ssum_wgts, ssamples, swtd_int_sum):
+          sxi, sxin, svol, sd, sbox, sbin, sjac,
+          ssum_wgts, ssamples, swtd_int_sum):
 
     for i in range(dim):
         if xu[i] <= xl[i]:
@@ -99,17 +42,37 @@ def vegas(dim, xl, xu, calls, stage, sbins, sdelx,
         ssamples = 0
 
     if stage <= 2:
-        bins = BINS_MAX
-        sboxes = 1
-        sjac = svol * bins ** dim / calls
 
-        if bins != sbins:
-            sbins = resize_grid(dim, sbins, bins, sxi, sxin)
+        sjac = svol * BINS_MAX ** dim / calls
+
+        if sbins != BINS_MAX:
+            # resize grid
+            pts_per_bin = sbins / BINS_MAX
+            for j in range(dim):
+                i = 1
+                xnew = 0
+                dw = 0
+                for k in range(1, sbins+1):
+                    dw += 1.0
+                    xold = xnew
+                    xnew = sxi[k, j]
+
+                    while dw > pts_per_bin:
+                        dw -= pts_per_bin
+                        sxin[i] = xnew - (xnew - xold) * dw
+                        i += 1
+
+                for k in range(1, BINS_MAX):
+                    sxi[k, j] = sxin[k]
+
+                sxi[BINS_MAX, j] = 1
+            sbins = BINS_MAX
 
     cum_int = 0.0
     cum_sig = 0.0
 
     iterations = 5
+    x = np.zeros(dim)
     for it in range(iterations):
         intgrl = 0
         tss = 0
@@ -121,35 +84,42 @@ def vegas(dim, xl, xu, calls, stage, sbins, sdelx,
         for i in range(dim):
             sbox[i] = 0
 
-        while True:
-            m = 0
-            q = 0
-            f_sq_sum = 0.0
+        m = 0
+        q = 0
+        f_sq_sum = 0.0
 
-            for k in range(calls):
+        for k in range(calls):
 
-                bin_vol = random_point(sx, sbin, dim, sbox, sbins, xl, sdelx, sxi)
+            bin_vol = 1
+            for i in range(dim):
+                z = (sbox[i] + np.random.uniform(0,1)) * sbins
+                w = nb.int64(z)
+                sbin[i] = w
+                if z == 0:
+                    bin_width = sxi[1, i]
+                    y = z * bin_width
+                else:
+                    bin_width = sxi[w+1, i] - sxi[w, i]
+                    y = sxi[w, i] + (z - w) * bin_width
+                x[i] = xl[i] + y * sdelx[i]
+                bin_vol *= bin_width
 
-                fval = sjac * bin_vol * MC_INTEGRAND(sx)
+            fval = sjac * bin_vol * MC_INTEGRAND(x)
 
-                d = fval - m
-                m += d / (k + 1.0)
-                q += d * d * (k / (k + 1.0))
+            d = fval - m
+            m += d / (k + 1.0)
+            q += d * d * (k / (k + 1.0))
 
-                f_sq = fval * fval
+            f_sq = fval * fval
 
-                # accumulate distribution
-                for j in range(dim):
-                    i = sbin[j]
-                    sd[i, j] += f_sq
+            # accumulate distribution
+            for j in range(dim):
+                i = sbin[j]
+                sd[i, j] += f_sq
 
-            intgrl += m * calls
-            f_sq_sum = q * calls
-            tss += f_sq_sum
-
-            if not change_box_coord(dim, sbox, sboxes):
-                break
-
+        intgrl += m * calls
+        f_sq_sum = q * calls
+        tss += f_sq_sum
         var = tss / (calls - 1)
 
         if var > 0:
@@ -160,6 +130,7 @@ def vegas(dim, xl, xu, calls, stage, sbins, sdelx,
             wgt = 0.0
 
         if wgt > 0.0:
+            ssamples+=1
             ssum_wgts += wgt
             swtd_int_sum += intgrl * wgt
             cum_int = swtd_int_sum / ssum_wgts
@@ -181,11 +152,9 @@ class make_vegas:
         self.sxi = np.zeros(shape=(BINS_MAX+1, dim))
         self.sxin = np.zeros(shape=(BINS_MAX+1))
         self.svol = 0
-        self.sx = np.zeros(dim)
         self.sd = np.zeros(shape=(BINS_MAX, dim))
         self.sbox = np.zeros(dim, dtype=np.int64)
         self.sjac = 0
-        self.sboxes = 0
         self.ssum_wgts = 0
         self.ssamples = 0
         self.swtd_int_sum = 0
@@ -193,7 +162,7 @@ class make_vegas:
     def integrate(self, xl, xu, calls, stage):
         r = vegas(self.dim, xl, xu, calls, stage,
                 self.sbins, self.sdelx, self.sxi, self.sxin, self.svol,
-                self.sx, self.sd, self.sbox, self.sbin, self.sjac, self.sboxes,
+                self.sd, self.sbox, self.sbin, self.sjac,
                 self.ssum_wgts, self.ssamples, self.swtd_int_sum)
         self.svol = r[0]
         return r[1:]
