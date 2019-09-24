33f45109721e793558e610ba68a045055996d228
Stefano Carrazza <stefano.carrazza@cern.ch>
2019-09-24 17:46:33 +0200

    removing unused functions

--- fpga/host.cc
+++ fpga/host.cc
@@ -10,20 +10,6 @@
 #define BINS_MAX 50
 #define ALPHA 1.5
 
-double lepage_test(double x[], int n) {
-    double a = 0.1;
-    double pref = pow(1.0/a/sqrt(M_PI), n);
-    double coef = 0.0;
-    for (int i = 1; i <= 100*n; i++) {
-        coef += (float) i;
-    }
-    for (int i = 0; i < n; i++) {
-        coef += pow((x[i] - 1.0/2.0)/a, 2);
-    }
-    coef -= 100.0*n*(100.0*n+1.0)/2.0;
-    return pref*exp(-coef);
-}
-
 double internal_rand(){
     double x = (double) rand()/RAND_MAX;
     return x;
